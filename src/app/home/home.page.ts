0cada47ea8664403cde2e6c78da06b75c1c46b00
José Augusto <joseaugustogomessantos12@gmail.com>
2024-11-05 21:58:59 -0300

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  videoElement: HTMLVideoElement | undefined;
+  canvasElement: HTMLCanvasElement | undefined;
+  context: CanvasRenderingContext2D | null = null;
+  imageUrl: string | undefined;
 
-  constructor() {}
+  ngOnInit() {
+    this.videoElement = document.getElementById('video') as HTMLVideoElement;
+    this.canvasElement = document.createElement('canvas');
+    this.context = this.canvasElement.getContext('2d');
+  }
 
+  startCamera() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (this.videoElement) {
+          this.videoElement.srcObject = stream;
+          this.videoElement.play();
+        }
+      })
+      .catch((err) => {
+        console.error('Erro ao acessar a webcam: ', err);
+      });
+  }
+
+  takePicture() {
+    if (this.context && this.canvasElement && this.videoElement) {
+      this.canvasElement.width = this.videoElement.videoWidth;
+      this.canvasElement.height = this.videoElement.videoHeight;
+
+      this.context.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      this.imageUrl = this.canvasElement.toDataURL('image/png');
+      console.log(this.imageUrl);
+    }
+  }
 }
